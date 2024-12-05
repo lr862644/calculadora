5bd5d324020ec55099aaea3b7242149e9c25bbce
leandro.reis <lr862644@gmail.com>
2024-12-05 10:53:37 -0300

    My First Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        //como criar uma variavel
+        // 1 tipo: integer, Double, Float
+        // 2 Nome qualquer nome que faça sentido
+
+        Double n1 = 4.0;
+        Double n2 = 6.0;
+        Double n3 = 6.0;
+        Double n4 = 5.0;
+
+        Double resultadoMedia;
+
+
+        Integer resultadoSoma;
+
+        resultadoMedia = variaveis.calculaMedia(n1, n2, n3, n4);
+        System.out.println(resultadoMedia);
+    }
+}
---
+++ src/main/java/org/example/variaveis.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class variaveis {
+
+
+    // como criar um metodo?
+    // 1 Restrição public, private,protectd
+    //002
+    // 2 Retorno: void, integer,Double, float, Boolean
+    // 3 Nome: define o nome do metodo
+    // 4 ():Parenteses
+    // 5 {}: Determina onde o metodo inicia e onde termina
+
+    public static Integer somaNumeros(Integer x, Integer y){
+
+        return x + y;
+
+    }
+    public static Double calculaMedia(Double n1,  Double n2, Double n3, Double n4){
+
+        return (n1 + n2 + n3 + n4) / 4;
+
+    }
+
+}
